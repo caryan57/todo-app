83b741e29b50a56af223daa2a2adce2e30ad1dde
Carlos Olivares <focus.olivares@gmail.com>
2022-08-01 13:07:04 -0500

    Ordenando el código de las funciones addTask y createTask, a su vez haciendo los comentarios adecuados

--- components/addTask.js
+++ components/addTask.js
@@ -2,27 +2,22 @@ import checkComplete from "./checkComplete.js";
 import deleteIcon from "./deleteIcon.js";
 
 const addTask = (e) => {
+  //Evitar comportamiento default del formulario
   e.preventDefault();
-  const list = document.querySelector("[data-list]");
-  const task = createTask(e);
-  list.appendChild(task);
-};
 
-const createTask = (e) => {
-  const taskList = JSON.parse(localStorage.getItem("tasks")) || [];
+  //Seleccionar elementos del DOM
+  const list = document.querySelector("[data-list]");
   const input = document.querySelector("[data-input-form]");
   const inputDate = document.querySelector("[data-input-date]");
+
+  //Dar formato a los valores de cada input
+  const value = input.value;
   const date = inputDate.value;
   const dateFormat = moment(date).format("DD/MM/YYYY");
-  const dateElement = document.createElement("span");
-  dateElement.innerHTML = dateFormat;
 
-  const value = input.value;
-  input.value = "";
+  //Guardar los valores en localStorage
+  const taskList = JSON.parse(localStorage.getItem("tasks")) || [];
 
-  const task = document.createElement("li");
-
-  const taskContent = document.createElement("div");
   const taskObj = {
     value,
     dateFormat,
@@ -31,16 +26,43 @@ const createTask = (e) => {
   taskList.push(taskObj);
   localStorage.setItem("tasks", JSON.stringify(taskList));
 
+  //Enviar el contenido de cada input como objeto
+  const task = createTask(taskObj);
+
+  //Agregar la tarea a la lista de tareas
+  list.appendChild(task);
+
+  //Resetear valores de los input
+  input.value = "";
+  inputDate.value = "";
+};
+
+const createTask = ({ value, dateFormat }) => {
+  //Crear span con la fecha
+  const dateElement = document.createElement("span");
+  dateElement.innerHTML = dateFormat;
+
+  //Crear elemento <li> que contiene un <div> con los sus respectivos iconos
+  const task = document.createElement("li");
+  const taskContent = document.createElement("div");
   taskContent.appendChild(checkComplete());
+
+  //Crear span con los datos de la tarea
   const titleTask = document.createElement("span");
   titleTask.classList.add("task");
   titleTask.innerText = value;
 
+  //Agregar el contenido del span al div
   taskContent.appendChild(titleTask);
+
+  //Agregar al elemento <li> todo lo creado previamente
   task.appendChild(taskContent);
   task.appendChild(dateElement);
+
+  //Crear icono de eliminar tarea
   task.appendChild(deleteIcon());
 
+  //Agregar clase "card" a la tarea
   task.classList.add("card");
 
   return task;
