15946105b31bbab19e2c3a48528768ee648bbe6d
Carlos Olivares <focus.olivares@gmail.com>
2022-08-01 10:54:00 -0500

    Almacenar en una variable el valor de la fecha

--- script.js
+++ script.js
@@ -6,16 +6,14 @@ const btn = document.querySelector("[data-form-btn]");
 const createTask = (e) => {
   e.preventDefault();
   const input = document.querySelector("[data-input-form]");
+  const inputDate = document.querySelector("[data-input-date]");
+  const date = inputDate.value;
   const value = input.value;
   input.value = "";
-
   const list = document.querySelector("[data-list]");
-
   const task = document.createElement("li");
-
   const taskContent = document.createElement("div");
   taskContent.appendChild(checkComplete());
-
   const titleTask = document.createElement("span");
   titleTask.classList.add("task");
   titleTask.innerText = value;
