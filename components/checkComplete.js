4ed3fe2911a4803d52b540b142bf4379e6e94ee2
Carlos Olivares <focus.olivares@gmail.com>
2022-08-01 09:53:22 -0500

    Iniciando con  proyecto base

---
+++ components/checkComplete.js
@@ -0,0 +1,16 @@
+const checkComplete = () => {
+  const i = document.createElement("i");
+  i.classList.add("far", "fa-check-square", "icon");
+
+  i.addEventListener("click", completeTask);
+
+  return i;
+};
+
+const completeTask = (e) => {
+  const element = e.target;
+  element.classList.toggle("far");
+  element.classList.toggle("fas");
+};
+
+export default checkComplete;
---
+++ script.js
@@ -0,0 +1,32 @@
+import checkComplete from "./components/checkComplete.js";
+import deleteIcon from "./components/deleteIcon.js";
+
+const btn = document.querySelector("[data-form-btn]");
+
+const createTask = (e) => {
+  e.preventDefault();
+  const input = document.querySelector("[data-input-form]");
+  const value = input.value;
+  input.value = "";
+
+  const list = document.querySelector("[data-list]");
+
+  const task = document.createElement("li");
+
+  const taskContent = document.createElement("div");
+  taskContent.appendChild(checkComplete());
+
+  const titleTask = document.createElement("span");
+  titleTask.classList.add("task");
+  titleTask.innerText = value;
+
+  taskContent.appendChild(titleTask);
+
+  task.appendChild(taskContent);
+  task.appendChild(deleteIcon());
+
+  task.classList.add("card");
+  list.appendChild(task);
+};
+
+btn.addEventListener("click", createTask);
